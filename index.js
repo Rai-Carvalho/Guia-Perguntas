09f1c6706e91262525e031bb1067e04ed89395a6
Rai-Carvalho <raiarturc@gmail.com>
2022-03-25 21:39:34 -0400

    Sequelize

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const perguntaModel = require("./database/Pergunta")
+const Pergunta = require("./database/Pergunta")
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    })
+
 //Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs')
 app.use(express.static('public'))
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-app.get("/",(req, res) => {
-    res.render("index.ejs")
+//Rotas
+app.get("/",(req, res) => { 
+    Pergunta.findAll({raw: true, order:[
+        ['id','DESC']
+    ]}).then(perguntas => {
+        res.render("index.ejs", {
+            perguntas: perguntas
+        })
+    })
+   
 })
 
 app.get("/perguntar", (req,res) => {
@@ -13,7 +39,14 @@ app.get("/perguntar", (req,res) => {
 })
 
 app.post("/salvarpergunta", (req,res) =>{
-    res.send("Formulário recebido")
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/")  
+    })
 })
 
 app.listen(8080,()=>{console.log("App rodando!")})
